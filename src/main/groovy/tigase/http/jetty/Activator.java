fe015e9a27905a29ed1918125db0378e5c6b103a
Andrzej Wójcik <andrzejw@tigase.org>
2014-07-08 13:43:47 +0200

    Moved Jetty HTTP Server dependent classes to separate project

---
+++ src/main/groovy/tigase/http/jetty/Activator.java
@@ -0,0 +1,109 @@
+/*
+ * Tigase HTTP API
+ * Copyright (C) 2004-2014 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.http.jetty;
+
+import java.util.logging.Logger;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import tigase.osgi.ModulesManager;
+
+public class Activator implements BundleActivator, ServiceListener {
+	
+    private static final Logger log = Logger.getLogger(Activator.class.getCanonicalName());
+
+    private static BundleContext context;
+    private ModulesManager serviceManager;
+    private ServiceReference serviceReference;
+
+	public static BundleContext getContext() {
+		return context;
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// Add ApiMessaceReceiver
+		Activator.context = context;
+        context.addServiceListener(this, "(&(objectClass=" + ModulesManager.class.getName() + "))");
+        serviceReference = context.getServiceReference(ModulesManager.class.getName());
+        if (serviceReference != null) {
+            serviceManager = (ModulesManager) context.getService(serviceReference);
+            registerAddons();
+        }
+	}
+
+	@Override
+	public void stop(BundleContext bc) throws Exception {
+        if (serviceManager != null) {
+            // unregister component and release Jetty HTTP Server
+            unregisterAddons();
+            context.ungetService(serviceReference);
+            serviceManager = null;
+            serviceReference = null;
+        }	
+	}
+
+    @Override
+    public void serviceChanged(ServiceEvent event) {
+        // handle case when Jetty is started or stopped during lifetime of started Tigase HTTP API component
+        if (event.getType() == ServiceEvent.REGISTERED) {
+            if (serviceReference == null) {
+                serviceReference = event.getServiceReference();
+                serviceManager = (ModulesManager) context.getService(serviceReference);
+                registerAddons();
+            }
+        }
+        else if (event.getType() == ServiceEvent.UNREGISTERING) {
+            if (serviceReference == event.getServiceReference()) {
+                unregisterAddons();
+                context.ungetService(serviceReference);
+                serviceManager = null;
+                serviceReference = null;
+            }
+        }
+    }
+	
+    /**
+     * Register ServerComponents
+     */
+    private void registerAddons() {
+        if (serviceManager != null) {
+            serviceManager.registerClass(JettyStandaloneHttpServer.class);
+			serviceManager.registerClass(JettyOSGiHttpServer.class);
+            serviceManager.update();
+        }
+    }
+
+    /**
+     * Unregister ServerComponents
+     */
+    private void unregisterAddons() {
+        if (serviceManager != null) {
+            serviceManager.unregisterClass(JettyStandaloneHttpServer.class);
+			serviceManager.unregisterClass(JettyOSGiHttpServer.class);
+			serviceManager.update();
+        }
+    }
+	
+}
---
+++ src/main/groovy/tigase/http/jetty/security/TigasePlainLoginService.groovy
@@ -0,0 +1,114 @@
+/*
+ * Tigase HTTP API
+ * Copyright (C) 2004-2013 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.http.jetty.security
+
+import org.eclipse.jetty.security.DefaultIdentityService
+import org.eclipse.jetty.security.IdentityService
+import org.eclipse.jetty.security.LoginService
+import org.eclipse.jetty.security.MappedLoginService
+import org.eclipse.jetty.server.UserIdentity
+import org.eclipse.jetty.util.security.Password
+import tigase.http.HttpServer
+import tigase.http.api.Service
+import tigase.xmpp.BareJID
+
+import javax.security.auth.Subject
+import java.security.Principal
+import java.util.logging.Level
+import java.util.logging.Logger
+
+public class TigasePlainLoginService implements LoginService {
+
+    private static final Logger log = Logger.getLogger(TigasePlainLoginService.class.getCanonicalName());
+
+    def identityService = new DefaultIdentityService();
+
+	Service service = null;
+	
+	public TigasePlainLoginService(Service service) {
+		this.service = service;
+	}
+	
+    @Override
+    String getName() {
+        return "TigasePlain";
+    }
+
+    @Override
+    UserIdentity login(String s, Object o) {
+        String cred = null;
+        if (o instanceof String) cred = (String) o;
+        if (o instanceof Password) cred = ((Password) o).toString();
+
+        if (cred != null) {
+            // decode credential
+            BareJID jid = BareJID.bareJIDInstance(s);
+
+            // authenticate using Tigase authentication repository
+            boolean authOk = false;
+            try { authOk = getService().getAuthRepository().plainAuth(jid, cred); } catch (ex) {  log.log(Level.FINE, "not authorized used = " + jid, ex); }
+
+            // we are authenticated so set correct authentication principal
+            if (authOk) {
+                Principal p = new MappedLoginService.KnownUser(s, null);
+
+                Subject subject = new Subject();
+                subject.getPrincipals().add(p);
+                subject.setReadOnly();
+
+                def roles = ["user"];
+
+                // add admin role if user is in admins list
+                if (getService().isAdmin(jid)) roles.add("admin");
+
+                return getIdentityService().newUserIdentity(subject, p, roles.toArray(new String[0]));
+            }
+        }
+
+        return null  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    boolean validate(UserIdentity userIdentity) {
+        // validate if user identity is valid
+        return getService().getUserRepository().getUserUID(BareJID.bareJIDInstance(userIdentity.getUserPrincipal().getName())) > 0;
+    }
+
+    @Override
+    IdentityService getIdentityService() {
+        return identityService;
+    }
+
+    @Override
+    void setIdentityService(IdentityService identityService2) {
+        identityService = identityService2;
+    }
+
+    @Override
+    void logout(UserIdentity userIdentity) {
+    }
+
+    Service getService() {
+        return service;
+    }
+
+}
