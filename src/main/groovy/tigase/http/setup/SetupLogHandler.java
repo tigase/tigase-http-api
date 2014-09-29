481c971fab2f094a5916665c8e8887d426a4e20d
Andrzej Wójcik <andrzejw@tigase.org>
2014-09-29 21:39:02 +0200

    Issue #2101 - rebased setup to use DBSchemaLoader for loading of base schema

---
+++ src/main/groovy/tigase/http/setup/SetupLogHandler.java
@@ -0,0 +1,58 @@
+/*
+ * Tigase HTTP API
+ * Copyright (C) 2004-2014 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.http.setup;
+
+import java.util.ArrayDeque;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ *
+ * @author andrzej
+ */
+public class SetupLogHandler extends Handler {
+
+	private final ArrayDeque<LogRecord> queue = new ArrayDeque<LogRecord>();
+	
+	public SetupLogHandler() {
+	}
+	
+	@Override
+	public void publish(LogRecord record) {
+		queue.offer(record);
+	}
+
+	@Override
+	public void flush() {
+		queue.clear();
+	}
+
+	@Override
+	public void close() throws SecurityException {
+		flush();
+	}
+	
+	public LogRecord poll() {
+		return queue.poll();
+	}
+	
+}
