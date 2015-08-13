4e1671647bc359ee405d23fa2379e3b90fb811ed
Andrzej Wójcik <andrzejw@tigase.org>
2015-08-13 23:41:15 +0200

    Issue #3375 - Adding support for delete user REST API to disconnect user in cluster mode

--- src/scripts/groovy/tigase/rest/user/UserAdmin.groovy
+++ src/scripts/groovy/tigase/rest/user/UserAdmin.groovy
@@ -1,4 +1,5 @@
 package tigase.rest.user
+
 /*
  * Tigase HTTP API
  * Copyright (C) 2004-2013 "Tigase, Inc." <office@tigase.com>
@@ -21,7 +22,11 @@ package tigase.rest.user
  * $Date$
  */
 import tigase.http.rest.Service
+import tigase.server.Iq
+import tigase.server.Packet
+import tigase.xml.Element
 import tigase.xmpp.BareJID
+import tigase.xmpp.StanzaType
 
 /**
  * Class implements ability to manage users for service administrator
@@ -32,6 +37,9 @@ import tigase.xmpp.BareJID
  */
 class UserAdminHandler extends tigase.http.rest.Handler {
 
+	 def TIMEOUT = 30 * 1000;
+	 def COMMAND_XMLNS = "http://jabber.org/protocol/commands";
+	
     public UserAdminHandler() {
 		description = [
 			regex : "/{user_jid}",
@@ -72,7 +80,7 @@ Example response:
         regex = /\/([^@\/]+)@([^@\/]+)/
 		authRequired = { api_key -> return api_key == null && requiredRole != null }
         requiredRole = "admin"
-        isAsync = false
+        isAsync = true
         execGet = { Service service, callback, user, localPart, domain ->
             def jid = BareJID.bareJIDInstance(localPart, domain);
             def uid = service.getUserRepository().getUserUID(jid);
@@ -103,8 +111,49 @@ Example response:
         execDelete = { Service service, callback, user, localPart, domain ->
             def jid = BareJID.bareJIDInstance(localPart, domain);
             def uid = service.getUserRepository().getUserUID(jid);
-            service.getAuthRepository().removeUser(jid)
-            callback([user:[jid:"$localPart@$domain", domain:domain, uid:uid]]);
+            
+			Element iq = new Element("iq");
+            iq.setAttribute("to", "sess-man@" + domain);
+			if (user != null && user != "null") {
+				iq.setAttribute("from", user.toString());
+			}
+            iq.setAttribute("type", "set");
+            iq.setAttribute("id", UUID.randomUUID().toString())
+
+            Element command = new Element("command");
+            command.setXMLNS(COMMAND_XMLNS);
+            command.setAttribute("node", "http://jabber.org/protocol/admin#delete-user");
+            iq.addChild(command);
+
+			Element x = new Element("x");
+			x.setXMLNS("jabber:x:data");
+			x.setAttribute("type", "submit");
+			command.addChild(x);
+			
+			Element fieldEl = new Element("field");
+			fieldEl.setAttribute("var", "notify-cluster");
+			fieldEl.addChild(new Element("value", "true"));
+			x.addChild(fieldEl);
+			fieldEl = new Element("field");
+			fieldEl.setAttribute("var", "accountjids");
+			fieldEl.addChild(new Element("value", jid.toString()));
+			x.addChild(fieldEl);
+			
+			service.sendPacket(new Iq(iq), TIMEOUT, { Packet result ->
+                if (result == null || result.getType() == StanzaType.error) {
+                    callback(null);
+                    return;
+                }
+				
+				command = result.getElement().getChild("command", COMMAND_XMLNS);
+				def noteEl = command.getChildren().find{ it.getName() == "x" }.getChildren().find{ it.getAttribute("var") == "Notes" };
+				if (noteEl == null) {
+					callback(null);
+					return;
+				}
+				
+				callback([user:[jid:"$localPart@$domain", domain:domain, uid:uid]]);
+			});
         }
         execPost = { Service service, callback, user, content, localPart, domain ->
             def jid = BareJID.bareJIDInstance(localPart, domain);
