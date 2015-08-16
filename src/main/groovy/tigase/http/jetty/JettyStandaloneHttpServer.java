7de8f907441f57098782c89217a7db426fdb5c8d
Andrzej Wójcik <andrzejw@tigase.org>
2015-08-16 21:46:04 +0200

    Issue #3392 - Add SSL support for HTTP REST API

--- src/main/groovy/tigase/http/jetty/JettyStandaloneHttpServer.java
+++ src/main/groovy/tigase/http/jetty/JettyStandaloneHttpServer.java
@@ -19,15 +19,25 @@
 
 package tigase.http.jetty;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.SSLContext;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import static tigase.http.api.HttpServerIfc.DEF_HTTP_PORT_VAL;
+import static tigase.http.api.HttpServerIfc.HTTP_PORTS_KEY;
 import static tigase.http.api.HttpServerIfc.HTTP_PORT_KEY;
+import static tigase.http.api.HttpServerIfc.PORT_DOMAIN_KEY;
+import static tigase.http.api.HttpServerIfc.PORT_SOCKET_KEY;
+import tigase.io.SSLContextContainerIfc;
+import tigase.io.TLSUtil;
+import tigase.net.SocketType;
 
 /**
  * This implementation embeds Jetty HTTP Server by starting separate instance
@@ -39,8 +49,9 @@ public class JettyStandaloneHttpServer extends AbstractJettyHttpServer {
 
 	private static final Logger log = Logger.getLogger(JettyStandaloneHttpServer.class.getCanonicalName());
 	
-	private int port = DEF_HTTP_PORT_VAL;
 	private Server server = null;
+	private int[] ports = { DEF_HTTP_PORT_VAL };
+	private final Map<String,Map<String,Object>> portsConfigs = new HashMap<>();
 	private final ContextHandlerCollection contexts = new ContextHandlerCollection();
 	
 	@Override
@@ -68,8 +79,24 @@ public class JettyStandaloneHttpServer extends AbstractJettyHttpServer {
 		if (server != null && (server.isStarted() || server.isStarting())) {
 			stop();
 		}
-		if (server == null)
-			server = new Server(port);
+		if (server == null) {
+			server = new Server();
+			for (int port : ports) {
+				Map<String,Object> config = portsConfigs.get(String.valueOf(port));
+				ServerConnector connector = null;
+				if (config == null || ((SocketType) config.get(PORT_SOCKET_KEY)) == SocketType.plain) {
+					connector = new ServerConnector(server);
+				} else {
+					String domain = (String) config.get(PORT_DOMAIN_KEY);
+					SSLContext context = TLSUtil.getSSLContext("TLS", domain);
+					SslContextFactory contextFactory = new SslContextFactory();
+					contextFactory.setSslContext(context);
+					connector = new ServerConnector(server, contextFactory);
+				}
+				connector.setPort(port);
+				server.addConnector(connector);				
+			}
+		}
 		server.setHandler(contexts);
 		try {
 			server.start();
@@ -95,7 +122,19 @@ public class JettyStandaloneHttpServer extends AbstractJettyHttpServer {
 	@Override
 	public void setProperties(Map<String, Object> props) {
 		if (props.containsKey(HTTP_PORT_KEY)) {
-			port = (Integer) props.get(HTTP_PORT_KEY);
+			ports = new int[] { (Integer) props.get(HTTP_PORT_KEY) };
+		}
+		if (props.containsKey(HTTP_PORTS_KEY)) {
+			ports = (int[]) props.get(HTTP_PORTS_KEY);
+		}
+		portsConfigs.clear();
+		for (int port : ports) {
+			Map<String,Object> config = new HashMap<>();
+			String socket = (String) props.get(String.valueOf(port) + "/" + PORT_SOCKET_KEY);
+			config.put(PORT_SOCKET_KEY, socket != null ? SocketType.valueOf(socket) : SocketType.plain);
+			String domain = (String) props.get(String.valueOf(port) + "/" + PORT_DOMAIN_KEY);
+			config.put(PORT_DOMAIN_KEY, domain);
+			portsConfigs.put(String.valueOf(port), config);
 		}
 	}
 	
