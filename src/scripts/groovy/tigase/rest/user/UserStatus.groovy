baf1105938ffa2f47394edc6c61b245e60cabb57
Wojciech Kapcia <wojtek@tigase.org>
2019-01-21 16:19:56 -0300

    #8483 update licence headers

--- src/scripts/groovy/tigase/rest/user/UserStatus.groovy
+++ src/scripts/groovy/tigase/rest/user/UserStatus.groovy
@@ -1,8 +1,6 @@
-/*
- * UserStatus.groovy
- *
- * Tigase HTTP API
- * Copyright (C) 2004-2018 "Tigase, Inc." <office@tigase.com>
+/**
+ * Tigase HTTP API component - Tigase HTTP API component
+ * Copyright (C) 2013 Tigase, Inc. (office@tigase.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -16,9 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- *
  */
-
 package rest.user
 
 import tigase.http.rest.Service
