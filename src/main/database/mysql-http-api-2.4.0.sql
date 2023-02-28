287eb9cd5a3cfc72fa520f89a188c1b7acb9c64e
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2023-02-28 10:55:43 +0100

    Allowed storage of unicode chars in uploaded files filenames #http-125

--- src/main/database/mysql-http-api-2.4.0.sql
+++ src/main/database/mysql-http-api-2.4.0.sql
@@ -16,3 +16,24 @@
 -- If not, see http://www.gnu.org/licenses/.
 --
 
+-- QUERY START:
+alter table tig_hfu_slots modify filename varchar(255) character set utf8mb4 not null;
+-- QUERY END:
+
+-- QUERY START:
+drop procedure  if exists Tig_HFU_AllocateSlot;
+-- QUERY END:
+
+delimiter //
+
+-- QUERY START:
+create procedure  Tig_HFU_AllocateSlot(_slotId varchar(60) CHARSET utf8, _uploader varchar(2049) CHARSET utf8, _domain varchar(1024) CHARSET utf8, _res varchar(1024) CHARSET utf8, _filename1 varchar(255) CHARSET utf8mb4, _filesize bigint, _contentType varchar(128) CHARSET utf8)
+begin
+    insert into tig_hfu_slots( slot_id, uploader, domain, res, filename, filesize, content_type, ts, status )
+        values (_slotId, _uploader, _domain, _res, _filename1, _filesize, _contentType, now(6), 0);
+
+    select _slotId as slotId;
+end //
+-- QUERY END:
+
+delimiter ;
